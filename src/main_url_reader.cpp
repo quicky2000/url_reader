214db5b2c288a1047a832bf480c6feb30f512bae
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2018-04-04 23:14:38 +0200

    Improve test

--- src/main_url_reader.cpp
+++ src/main_url_reader.cpp
@@ -15,8 +15,17 @@
       along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 #ifdef URL_READER_SELF_TEST
+
+#include "url_reader.h"
+#include <iostream>
+
 int main(void)
 {
+    quicky_url_reader::url_reader & l_instance = quicky_url_reader::url_reader::instance();
+    std::string l_page_content;
+    l_instance.dump_url("http://www.google.fr", l_page_content);
+    std::cout << "Page content :" << std::endl << l_page_content;
+
 }
 #endif // URL_READER_SELF_TEST
 // EOF
